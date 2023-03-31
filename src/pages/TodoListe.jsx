e9776793e4958ef54b93ceac4b8969a8c11a7050
Adnane2002 <adnanedouiab8@gmail.com>
2023-03-31 14:52:53 +0000

    add liste

--- src/pages/TodoListe.jsx
+++ src/pages/TodoListe.jsx
@@ -3,7 +3,30 @@ import React from 'react'
 export default function TodoListe(){
   return (
     <div>
-      To do liste
-    </div>
+<div className="col-md-6">
+                    <div className="card mt-5 shadow-sm">
+                        <div className="card-header">
+                            <h6>Liste des tâches</h6>
+                        </div>
+                        <div className="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <span>Tache</span>
+                                    <div>
+                                        <button className="btn btn-success btn-sm me-4">Terminer</button>
+                                        <button className="btn btn-danger btn-sm">Supprimer</button>
+                                    </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <span>Tache</span>
+                                    <div>
+                                        <button className="btn btn-success btn-sm me-4">Terminer</button>
+                                        <button className="btn btn-danger btn-sm">Supprimer</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>    </div>
   )
 }
